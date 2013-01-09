d57f1c84714cae1a020ff1efbb703b986a31c95b
mark.everline <mark.everline@gmail.com>
2013-01-09 19:59:06 +0000

    remove not used.

--- transiteRepostiroy/src/org/transiteRepositry/server/request/TransiteStopQueryRequest.java
+++
@@ -1,8 +0,0 @@
-package org.transiteRepositry.server.request;
-
-import com.thoughtworks.xstream.annotations.XStreamAlias;
-
-@XStreamAlias("TransiteStopQueryRequest")
-public class TransiteStopQueryRequest extends AbstractSpatialQuery {
-
-}
