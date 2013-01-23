2e23b8b40ed717fa0e5c274edb9ac65f6990114d
mark.everline@gmail.com <mark.everline@gmail.com@a4a4cf4b-0f58-a325-55e2-11dba026831d>
2013-01-23 20:46:11 +0000

    More code for transloc api.

--- trunk/scala-code/src/me/transite/feeds/transloc/Stop.scala
+++ trunk/scala-code/src/me/transite/feeds/transloc/Stop.scala
@@ -1,15 +1,61 @@
 package me.transite.feeds.transloc
 
-import me.utilities.Coordinate
+/*
+ * Transloc Version 1.1 Agenecies api inside Data array
+ * 
+ * URL
+ *   http://api.transloc.com/version/stops.json
+ * Rate Limit
+ *   Can be requested every 10 seconds from the same IP address.
+ * Required Parameters
+ *   agencies - a list of agency IDs which to retreive
+ * Optional Parameters
+ *   geo_area - a geographical area filter (see geo_area)
+ * 
+ * {
+ *   "api_current_version": "1.1",
+ *   "api_version": "1.1",
+ *   "generated_on": "2011-01-10T15:47:50+00:00",
+ *   "expires_in": 3600,
+ *   "rate_limit": 10,
+ *   "data": [
+ *       {
+ *           "stop_id": "stop_id",
+ *           "agency_id": "agency_id",
+ *           "name": "Livingston Ave",
+ *           "description": "A very high brow stop.",
+ *           "code": "livingston",
+ *           "url": "http://www.example.com/livingston-ave",
+ *           "location_type": "stop", # One of ['stop', 'station']
+ *           "location": {
+ *               "lat": -78.637339999999995,
+ *               "lng": 35.777357000000002
+ *           },
+ *           "routes": [
+ *               route_id
+ *           ]
+ *       }
+ *   ]
+ *}
+ * 
+ */
 
-class TransLocStop(stop_id:String, 
-				   agency_id:String, 
-				   name:String, 
-				   description:String, 
-				   code:String,
-				   url:String,
-				   locType:Enumeration,
-				   coord:Coordinate,
-				   routes:Array[String]) {
+import me.utilities.Coordinate
+import me.transite.feeds.transloc.utils.LocationType
+
+
+class Stop(val stop_id:String, 
+		   val agency_id:String, 
+		   val name:String, 
+		   val description:String, 
+		   val code:String,
+		   val url:String,
+		   val locType:LocationType,
+		   val coord:Coordinate,
+		   val routes:Array[String]) {
+  
+     def toUrlString() : String = {
+       stop_id
+   } 
 
 }
