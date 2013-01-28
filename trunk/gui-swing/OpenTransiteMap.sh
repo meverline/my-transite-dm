84f306eeec73f32360f94fb95fe585d7e11683af
mark.everline@gmail.com <mark.everline@gmail.com@a4a4cf4b-0f58-a325-55e2-11dba026831d>
2013-01-28 01:54:16 +0000

    Add check for Mac or windows for path seperator.

--- trunk/gui-swing/OpenTransiteMap.sh
+++ trunk/gui-swing/OpenTransiteMap.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/perl
 
+my $os = $^O;
+my $pathSep = ";";
+
+if ( $os == "darwin" ) {
+	$pathSep=":";
+}
+
 my $dir = 'lib';
 
 opendir(DIR, $dir) or die $!;
 
-my @classPath = split(";", $ENV{'CLASSPATH'});
+my @classPath = split($pathSep, $ENV{'CLASSPATH'});
 push(@classPath,"gui-swing-0.9.jar");
 while (my $file = readdir(DIR)) {
 	push(@classPath, "lib/$file");
 }
 
-$ENV{'CLASSPATH'} = join(";",@classPath);
+$ENV{'CLASSPATH'} = join($pathSep,@classPath);
 
-my @args=("java", "me.openMap.OpenTransitMap" );
+my @args=("java", "-Xmx2048m", "me.openMap.OpenTransitMap" );
 exec @args
+ 
