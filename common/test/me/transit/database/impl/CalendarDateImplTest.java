26b53a44ac5dc97afb17af98a634aba201d505af
mark.everline <mark.everline@gmail.com>
2013-08-16 09:59:06 +0000

    Update test and remove unneeded file.

--- common/test/me/transit/database/impl/CalendarDateImplTest.java
+++ common/test/me/transit/database/impl/CalendarDateImplTest.java
@@ -2,18 +2,54 @@ package me.transit.database.impl;
 
 import static org.junit.Assert.*;
 
+import java.util.Calendar;
+
+import me.transit.database.Agency;
+import me.transit.database.CalendarDate;
+
 import org.junit.Test;
 
 public class CalendarDateImplTest {
 
 	@Test
 	public void testConstructor() {
-		fail("Not yet implemented"); // TODO
+		CalendarDate cal = new CalendarDateImpl();
+		
+		assertNull(cal.getAgency());
+		assertEquals(-1, cal.getUUID());
+		assertNull(cal.getId());
+		assertEquals("0.5", cal.getVersion());
+		assertNull(cal.getDate());
+		assertEquals(CalendarDate.ExceptionType.UNKNOWN, cal.getExceptionType());
+		
 	}
 
 	@Test
 	public void testGetsAndSets() {
-		fail("Not yet implemented"); // TODO
+		Agency agency = new AgencyImpl("test");
+		CalendarDate cal = new CalendarDateImpl();
+		
+		assertNull(cal.getAgency());
+		assertEquals(-1, cal.getUUID());
+		assertNull( cal.getId());
+		assertEquals("0.5", cal.getVersion());
+		assertNull(cal.getDate());
+		assertEquals(CalendarDate.ExceptionType.UNKNOWN, cal.getExceptionType());
+		
+		Calendar now = Calendar.getInstance();
+		cal.setAgency(agency);
+		cal.setUUID(10);
+		cal.setId("id");
+	    cal.setVersion("1.0");
+	    cal.setDate( now);
+	    cal.setExceptionType(CalendarDate.ExceptionType.ADD_SERVICE);
+	    
+	    assertEquals(agency, cal.getAgency());
+	    assertEquals(10, cal.getUUID());
+	    assertEquals("id", cal.getId());
+	    assertEquals("1.0", cal.getVersion());
+	    assertEquals(now, cal.getDate());
+	    assertEquals(CalendarDate.ExceptionType.ADD_SERVICE, cal.getExceptionType());
 	}
 
 }
