c7ee7f502aeae5c5fb223234d043f2a12a338d1c
mark.everline@gmail.com <mark.everline@gmail.com@a4a4cf4b-0f58-a325-55e2-11dba026831d>
2013-08-20 02:06:10 +0000

    put under svn control

---
+++ trunk/common/test/me/transit/database/impl/RouteGeometryImplTest.java
@@ -0,0 +1,80 @@
+package me.transit.database.impl;
+
+import static org.junit.Assert.*;
+import me.transit.database.Agency;
+import me.transit.database.RouteGeometry;
+
+import org.junit.Test;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LineString;
+
+public class RouteGeometryImplTest {
+
+	/**
+	 * 
+	 */
+	@Test
+	public void testConstructor() {
+		RouteGeometry geo = new RouteGeometryImpl();
+		
+		assertNull(geo.getAgency());
+		assertNull(geo.getId());
+		assertNull(geo.getShape());
+		assertEquals(-1, geo.getUUID());
+		assertEquals("0.5", geo.getVersion());
+	}
+	
+	/**
+	 * 
+	 */
+	public static RouteGeometry createGeometry()
+	{
+		GeometryFactory factory = new GeometryFactory();
+		Agency agency = new AgencyImpl();
+		RouteGeometry geo = new RouteGeometryImpl();
+		
+		geo.setAgency(agency);
+		geo.setId("id");
+		geo.setUUID(10);
+		geo.setVersion("1.0");
+		
+		Coordinate array[] = new Coordinate[2];
+		array[0] = new Coordinate(10,10);
+		array[1] = new Coordinate(20,02);
+		
+		LineString poly =  factory.createLineString(array);
+		geo.setShape(poly);
+		return geo;
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void testGetAndSets() {
+		GeometryFactory factory = new GeometryFactory();
+		Agency agency = new AgencyImpl();
+		RouteGeometry geo = RouteGeometryImplTest.createGeometry();
+				
+		geo.setAgency(agency);
+		geo.setId("id");
+		geo.setUUID(10);
+		geo.setVersion("1.0");
+		
+		Coordinate array[] = new Coordinate[2];
+		array[0] = new Coordinate(10,10);
+		array[1] = new Coordinate(20,02);
+		
+		LineString poly =  factory.createLineString(array);
+		geo.setShape(poly);
+		
+		assertEquals(agency, geo.getAgency());
+		assertEquals("id", geo.getId());
+		assertEquals(poly, geo.getShape());
+		assertEquals(10, geo.getUUID());
+		assertEquals("1.0", geo.getVersion());
+	}
+
+}
---
+++ trunk/common/test/me/transit/database/impl/TransitStopImplTest.java
@@ -0,0 +1,77 @@
+package me.transit.database.impl;
+
+import static org.junit.Assert.*;
+import me.transit.database.Agency;
+import me.transit.database.TransitStop;
+
+import org.junit.Test;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Point;
+
+public class TransitStopImplTest {
+
+	/**
+	 * 
+	 */
+	@Test
+	public void testConstructor() {
+		TransitStop stop = new TransitStopImpl();
+		
+		assertNull(stop.getAgency());
+		assertEquals(-1, stop.getUUID());
+		assertNull(stop.getId());
+		assertEquals("0.5", stop.getVersion());
+		assertNull(stop.getZoneId());
+		assertNull(stop.getLocation());
+		assertEquals(TransitStop.LocationType.UNKNOW, stop.getLocationType());
+		assertEquals("", stop.getCode());
+		assertEquals("", stop.getDesc());
+		assertNull(stop.getName());
+		assertEquals(-1, stop.getParentStation());
+		assertEquals("", stop.getUrl());
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void testGetAndSet() {
+		GeometryFactory factory = new GeometryFactory();
+		Agency agency = new AgencyImpl("name");
+		TransitStop stop = new TransitStopImpl();
+		
+		stop.setAgency(agency);
+		stop.setUUID(10);
+		stop.setId("Id");
+		stop.setVersion("1.0");
+		
+		stop.setZoneId("zone");
+		stop.setCode("Code");
+		stop.setDesc("Description");
+		stop.setName("name");
+		stop.setParentStation(10);
+		stop.setUrl("Url");
+		stop.setLocationType(TransitStop.LocationType.STOP);
+		
+		Point pt = factory.createPoint(new Coordinate(10, 20));
+		stop.setLocation(pt);
+		
+		assertEquals(agency, stop.getAgency());
+		assertEquals(10, stop.getUUID());
+		assertEquals("Id", stop.getId());
+		assertEquals("1.0", stop.getVersion());
+		assertEquals("zone", stop.getZoneId());
+		assertEquals(pt, stop.getLocation());
+		assertEquals(TransitStop.LocationType.STOP, stop.getLocationType());
+		assertEquals("Code", stop.getCode());
+		assertEquals("Description", stop.getDesc());
+		assertEquals("name", stop.getName());
+		assertEquals(10, stop.getParentStation());
+		assertEquals("Url", stop.getUrl());
+	}
+	
+	
+
+}
