ed9e10a62a11073d0ab0e9ac2500f689de18df86
mark.everline@gmail.com <mark.everline@gmail.com@a4a4cf4b-0f58-a325-55e2-11dba026831d>
2013-10-13 19:02:14 +0000

    add graph functions and command line interface.

--- trunk/lisp/LandOfLispTextGame5.lisp
+++ trunk/lisp/LandOfLispTextGame5.lisp
@@ -1,4 +1,4 @@
-(defparameter *nodes* '((living-room (you are in the living-room. a wizard is snoring loudly on the couch.))
+(defparameter *wizard-nodes* '((living-room (you are in the living-room. a wizard is snoring loudly on the couch.))
                         (garden (you are in a beautifule garden. there is a well in front of your.))
                         (attic (you are in the attic. there is a giant welding torch in the corner.)))
 )
@@ -7,7 +7,7 @@
    (cadr (assoc location nodes))
 )
 
-(defparameter *edges* '((living-room (garden west door)
+(defparameter *wizard-edges* '((living-room (garden west door)
                                      (attic upstairs ladder))
                         (garden (living-room east door))
                         (attic (living-room downstairs ladder)))
@@ -44,14 +44,14 @@
 (defparameter *location* 'living-room)
 
 (defun look ()
-  (append (describe-location *location* *nodes*)
-          (describe-paths *location* *edges*)
+  (append (describe-location *location* *wizard-nodes*)
+          (describe-paths *location* *wizard-edges*)
           (describe-objects *location* *objects* *object-locations* ))
 )
 
 (defun walk (direction)
   (let ((next (find direction
-                    (cdr (assoc *location* *edges*))
+                    (cdr (assoc *location* *wizard-edges*))
                     :key #'cadr)))
     (if next 
         (progn (setf *location* (car next))
@@ -118,6 +118,97 @@
       (game-print (game-eval cmd))
       (game-repl)))
 )
+
+(defun dot-name (exp)
+  (substitute-if #\_ (complement #'alphanumericp)
+  (prin1-to-string exp))
+)
+
+(defparameter *max-label-length* 30)
+
+(defun dot-label (exp)
+  (if exp
+      (let ((s (write-to-string exp :pretty nil)))
+        (if (> (length s) *max-label-length*)
+            (concatenate 'string (subseq s 0 (- *max-label-length* 3)) "...")
+          s))
+    "")
+)
+
+(defun nodes->dot (nodes)
+  (mapc (lambda (node)
+          (fresh-line)
+          (princ (dot-name (car node)))
+          (princ "[label=\"")
+          (princ (dot-label node))
+          (princ "\"];"))
+        nodes)
+)
+
+(defun edges->dot (edges)
+  (mapc (lambda (node)
+        (mapc (lambda (edge)
+                (fresh-line)
+                (princ (dot-name (car node)))
+                (princ "->")
+                (princ (dot-name (car edge)))
+                (princ "[label=\"")
+                (princ (dot-label (cdr edge)))
+                (princ "\"];"))
+              (cdr node)))
+  edges)
+)
+
+(defun graph->dot (nodes edges)
+   (princ "digraph{")
+   (nodes->dot nodes)
+   (edges->dot edges)
+   (princ "}")
+)
+
+(defun dot->png (fname thunk)
+  (with-open-file (*standard-output*
+                   fname
+                   :direction :output
+                   :if-exists :supersede)
+    (funcall thunk))
+  (sys:run-shell-command (concatenate 'string "dot -Tpng -O " fname) :wait t )
+)
+
+(defun graph->png (fname nodes edges)
+  (dot->png fname
+           (lambda ()
+             (graph->dot nodes edges)))
+)
+
+(defun uedges->dot (edges)
+  (maplist (lambda (lst)
+             (mapc (lambda (edge)
+                     (unless (assoc (car edge) (cdr lst))
+                       (fresh-line)
+                       (princ (dot-name (caar lst)))
+                       (princ "--")
+                       (princ (dot-name (car edge)))
+                       (princ "[label=\"")
+                       (princ (dot-label (cdr edge)))
+                       (princ "\"];" )))
+                   (cdar lst)))
+             edges)
+)
+
+(defun ugraph->dot (nodes edges)
+  (princ "graph {" )
+  (nodes->dot nodes)
+  (uedges->dot edges)
+  (princ "}" )
+)
+
+(defun ugraph->png (fname nodes edges)
+  (dot->png fname
+            (lambda ()
+              (ugraph->dot nodes edges)))
+)
+
 
 
    
