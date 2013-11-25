e9b998605a1379a440c76023623a7e64b19cda8a
mark.everline <mark.everline@gmail.com>
2013-11-25 04:30:05 +0000

    Add comment to credit proper source.

--- common/src/me/math/grid/UniformSpatialGrid.java
+++ common/src/me/math/grid/UniformSpatialGrid.java
@@ -13,6 +13,9 @@
 //
 //    You should have received a copy of the GNU General Public License
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//    Based Upon C# code by Frank Levine and Paul Greene
+//    Adapted to Java by Mark Everline
 
 package me.math.grid;
 
