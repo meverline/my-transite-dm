94228b151b7e3b1578ffb5c56947922966af3ede
mark.everline@gmail.com <mark.everline@gmail.com@a4a4cf4b-0f58-a325-55e2-11dba026831d>
2013-11-25 04:30:05 +0000

    Add comment to credit proper source.

--- trunk/common/src/me/math/ECFPositionedFrame.java
+++ trunk/common/src/me/math/ECFPositionedFrame.java
@@ -13,6 +13,9 @@
 //
 //    You should have received a copy of the GNU General Public License
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//    Orginal C# code by Frank Levine and Paul Greene
+//    Traslated to Java by Mark Everline
 
 package me.math;
 
