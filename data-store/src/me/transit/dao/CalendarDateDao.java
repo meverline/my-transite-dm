3a3575a213ec1d26d4f2f85ce3c463fc4f8708af
Mark Everline <mark.everline@gmail.com>
2017-05-28 22:39:25 -0400

    move things around also move to using pom.xml and update version based upon what is in centeral repo for now.

--- data-store/src/me/transit/dao/CalendarDateDao.java
+++
@@ -1,8 +0,0 @@
-package me.transit.dao;
-
-import me.transit.database.CalendarDate;
-
-
-public interface CalendarDateDao extends TransitDao<CalendarDate> {
-
-}
--- data-store/src/me/transit/dao/ServiceDateDao.java
+++
@@ -1,8 +0,0 @@
-package me.transit.dao;
-
-import me.transit.database.ServiceDate;
-
-
-public interface ServiceDateDao extends TransitDao<ServiceDate> {
-
-}
