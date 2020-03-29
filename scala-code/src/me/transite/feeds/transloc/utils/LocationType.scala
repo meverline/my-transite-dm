73d8cad10a5f83160ea55069e68a34e5e83c9d0f
markeverline <mark.everline@gmail.com>
2020-03-29 12:32:30 -0400

    remove old file and junk data.

--- scala-code/src/me/transite/feeds/transloc/utils/LocationType.scala
+++
@@ -1,6 +0,0 @@
-package me.transite.feeds.transloc.utils
-
-
-sealed abstract class LocationType
-case object StopLocationTpe extends LocationType
-case object StationLocationType extends LocationType
--- scala-code/src/me/transite/feeds/transloc/utils/TrackingStatus.scala
+++
@@ -1,5 +0,0 @@
-package me.transite.feeds.transloc.utils
-
-sealed abstract class TrackingStatus
-case object Up extends TrackingStatus
-case object Down extends TrackingStatus
